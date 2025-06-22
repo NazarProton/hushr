be386728f0778559e70f1a35f709cfcaac7c5e63
Nazar Proton <nproton2012@gmail.com>
2025-06-22 03:34:54 +0200

    Add built files to main branch for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   root: '.',
-  base: '/hushr/',
+  base: '/',
   plugins: [react()],
   build: {
     rollupOptions: {
