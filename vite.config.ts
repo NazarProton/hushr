def3541f2063b88098c0f7f6964ca46b151bd033
Nazar Proton <nproton2012@gmail.com>
2025-06-22 03:24:44 +0200

    Update vite config base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   root: '.',
-  base: '/',
+  base: '/hushr/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
   build: {
     rollupOptions: {
       input: 'index.html',
