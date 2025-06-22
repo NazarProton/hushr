2a4d554c9ab2c3aa09d7564558a70774f8981669
Nazar Proton <nproton2012@gmail.com>
2025-06-22 03:58:21 +0200

    Fix base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   root: '.',
-  base: '/',
+  base: '/hushr/',
   plugins: [react()],
   build: {
     rollupOptions: {
