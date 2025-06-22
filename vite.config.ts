de39daa4e10b2ea198c0ae6fb7a7aef499ac12ae
Nazar Proton <nproton2012@gmail.com>
2025-06-22 02:33:54 +0200

    Add README and update GitHub Pages configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   root: '.',
-  base: '/hushr-social/',
+  base: '/project/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
