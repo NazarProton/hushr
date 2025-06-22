865c66ba1d1159a0d2ba4196e4a0d0b80bc361bd
Nazar Proton <nproton2012@gmail.com>
2025-06-22 02:34:24 +0200

    Fix base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   root: '.',
-  base: '/project/',
+  base: '/hushr-social/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
