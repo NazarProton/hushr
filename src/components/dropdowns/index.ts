fda078c7e941335bdf04d124b960664aa349f2bd
Nazar Proton <nproton2012@gmail.com>
2025-06-22 03:13:39 +0200

    Deploy to GitHub Pages

--- src/components/dropdowns/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as TokenDropdown } from './TokenDropdown';
-export { default as NetworkDropdown } from './NetworkDropdown';
-export { default as PrivacyDropdown } from './PrivacyDropdown';
-export { default as GasDropdown } from './GasDropdown';
